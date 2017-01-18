4a38ff1a0297aafb2a57a2bc2407a0c7736e2466
edwardjackchandler <edwardjackchandler@gmail.com>
2017-01-18 22:45:12 +0000

    Added Comments

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -4,20 +4,21 @@ import requests
 import tmdbsimple as tmdb
 import json
 
+#Api Key for The Movie DB
 tmdb.API_KEY = "030f4b9077820a1b72901fa807812b03"
-endOfCollection = 0
 
+#URL for a 'collection' (A group of movies on the TMDB website - eg. Star Wars movies, Indiana Jones films etc.)
 collectionURL = "https://api.themoviedb.org/3/collection/10194?language=en-US&api_key=030f4b9077820a1b72901fa807812b03"
 
-res = requests.get(collectionURL)
-
-payload = "{}"
-response = requests.request("GET", collectionURL, data=payload)
+#Use requests to get a response from
+response = requests.request("GET", collectionURL)
 
 collection = json.loads(response.text)
 
 movies = []
 
+#In the JSON object collection, 'parts' contains a list of movies.
+#Loop through these movies and add an instance of the class Movie for each JSON object
 for movie in collection['parts']:
 
     fullPosterPath = "https://image.tmdb.org/t/p/w1280" + movie['poster_path']
@@ -25,4 +26,5 @@ for movie in collection['parts']:
                                fullPosterPath,
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")))
 
+#Fresh Tomatoes do your thang
 fresh_tomatoes.open_movies_page(movies)
--- movies/media.py
+++ movies/media.py
@@ -1,11 +1,23 @@
 import webbrowser
 
+
 class Movie():
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube_url):
+        """Represents a movie.
+
+           Keyword arguments:
+           movie_title -- the title
+           movie_storyline -- the storyline
+           psoter_image -- link to an image of the movie poster
+           trailer_youtube_url -- url to the trailer of the movie on youtube
+           """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
+        """Opens a webpage with the youtube trailer
+        """
         webbrowser.open(self.trailer_youtube_url)
